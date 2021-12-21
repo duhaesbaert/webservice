929fc6a49b22a5e7ec0b3db3e9c56a91a7a8ad51
Eduardo Haesbaert <eduardo.haesbaert@gmail.com>
2021-12-21 11:37:23 -0300

    #21
    Implemented complete CRUD for Country object, and used the architecture developed for persisting all the information in memory to avoid consulting the database when no information was updated.

--- models/country.go
+++ models/country.go
@@ -16,46 +16,24 @@ type Country struct {
 }
 
 var (
-	countries     = make(map[int]*Country)
-	nextCountryID = 1
+	countries = make(map[int]*Country)
+	nextCountryID = updateCountriesInMemory()
+	currentCollection = "Countries"
 )
 
-func GetCountries() []Country {
-	client, err := db.OpenConnectionToMongo()
-
-	if err != nil {
-		return []Country{}
-	}
-
-	filter := bson.D{}
-	projection := bson.D{{"ID",1},{"Name", 1},{"Code", 1}}
-	opts := options.Find().SetProjection(projection)
-
-	coll := client.Database(db.GetDatabaseName()).Collection("Countries")
-	cursor, err := coll.Find(context.TODO(), filter, opts)
-
-	var results []bson.D
-
-	if err = cursor.All(context.TODO(), &results); err != nil {
-		panic(err)
-	}
-
-	defer db.CloseConnectionToMongo(client)
-
-	var listCountries []Country
-	for _, v := range results {
-		c := bsonToCountry(v)
-
-		//add object into hashmap
-		listCountries = append(listCountries, c)
-		//countries[i] = &c
-	}
-
-	return listCountries
+//In Memory: returns the complete list of countries that has been.
+//Returns a hashmap containing the list of countries
+func GetCountries() map[int]*Country {
+	return countries
 }
 
+//In Memory: Searches for a specific country on the hashmap.
+//Returns a country object and an error in case it was not possible to find the record
 func GetCountryByID(id int) (Country, error) {
-	client, err := db.OpenConnectionToMongo()
+	//Implementation consulting the databasse.
+	//This was replaced by persisting the list in memory and then updating the list when values on the database are updated.
+	//Possible only because all insert and updates done on the database pass go this class
+	/*client, err := db.OpenConnectionToMongo()
 
 	if err != nil {
 		return Country{}, nil
@@ -82,14 +60,18 @@ func GetCountryByID(id int) (Country, error) {
 		return c, nil
 	}
 
-	return Country{}, fmt.Errorf("Country with ID '%v' not found", id)
 
-	//Old Implementation with values persisted in memory
-	//if _, found := countries[id]; found {
-	//	return *countries[id], nil
-	//}
+	*/
+
+	if _, found := countries[id]; found {
+		return *countries[id], nil
+	}
+
+	return Country{}, fmt.Errorf("Country with ID '%v' not found", id)
 }
 
+//In DB: Creates a new country record to the collection and updates the countries in memory.
+//Returns a country object and an error in case it was not possible to create the record
 func AddCountry(c Country) (Country, error) {
 	if c.ID != 0 {
 		return Country{}, fmt.Errorf("Country must not include ID")
@@ -99,9 +81,9 @@ func AddCountry(c Country) (Country, error) {
 		return Country{}, fmt.Errorf("Country with CODE '%v' already exists", c.Code)
 	}
 
-	//c.ID = nextCountryID
-	//countries[nextCountryID] = &c
-	//nextCountryID++
+	c.ID = nextCountryID
+	countries[nextCountryID] = &c
+	nextCountryID++
 
 	//Validate if able to connect to MongoDB
 	client, err := db.OpenConnectionToMongo()
@@ -120,41 +102,88 @@ func AddCountry(c Country) (Country, error) {
 
 	defer db.CloseConnectionToMongo(client)
 
+	updateCountriesInMemory()
+
 	return c, nil
 }
 
+//In DB: Updates a country record on the collection and updates the countries in memory.
+//Returns a country object and an error in case it was not possible to update the record
 func UpdateCountry(c Country) (Country, error) {
 	if _, found := countries[c.ID]; found {
 		countries[c.ID] = &c
+
+		//establish connection to database
+		client, err := db.OpenConnectionToMongo()
+		if err != nil {
+			return Country{}, fmt.Errorf("Could not establish connection to database")
+		}
+
+		//create parameters for updating the values of the country
+		coll := client.Database(db.GetDatabaseName()).Collection(currentCollection)
+		filter := bson.D{{"ID", c.ID}}
+		update := bson.D{{"$set",bson.D{{"Name", c.Name},{"Code", c.Code}}}}
+
+		//execute update on the database record
+		_, err = coll.UpdateOne(context.TODO(), filter, update)
+		if err != nil {
+			return Country{}, fmt.Errorf("Could not update country on the database")
+		}
+
+		defer db.CloseConnectionToMongo(client)
+
+		//Update Candidate with the new value of country
+		for _, v := range GetCandidatesWithCountry(c.ID) {
+			v.CountryObj = c
+			UpdateCandidate(v)
+		}
+
+		//Update Job Requisition with new value of country
+		for _, v := range GetRequisitionsWithCountry(c.ID) {
+			v.JobReqCountry = c
+			UpdateJobRequisition(v)
+		}
+
+		//Update the list stored in memory
+		updateCountriesInMemory()
 		return c, nil
-	}
-
-	//Update Candidate with the new value of country
-	for _, v := range GetCandidatesWithCountry(c.ID) {
-		v.CountryObj = c
-		UpdateCandidate(v)
-
-	}
-
-	//Update Job Requisition with new value of country
-	for _, v := range GetRequisitionsWithCountry(c.ID) {
-		v.JobReqCountry = c
-		UpdateJobRequisition(v)
 	}
 
 	return Country{}, fmt.Errorf("Country to be updated not found")
 }
 
+//In DB: Removes a country record from the collection and updates the countries in memory.
+//Returns error if failed to complete the deletion on the DB
 func RemoveCountryByID(id int) error {
 	if _, found := countries[id]; found {
 		delete(countries, id)
+
+		client, err := db.OpenConnectionToMongo()
+		if err != nil {
+			return fmt.Errorf("Could not establish connection to database")
+		}
+
+		//create parameters for updating the values of the country
+		coll := client.Database(db.GetDatabaseName()).Collection(currentCollection)
+		filter := bson.D{{"ID", id}}
+
+		//Execute deletion on the database
+		_, err = coll.DeleteOne(context.TODO(), filter)
+		if err != nil {
+			return fmt.Errorf("Could not delete Country")
+		}
+
+		defer db.CloseConnectionToMongo(client)
+
+		//update the list stored in memory
+		updateCountriesInMemory()
 		return nil
 	}
 
 	return fmt.Errorf("Country with ID '%v' not found", id)
 }
 
-//Validate if the country with given ID already exists on the list
+//In Memory:Validate if the country with given ID already exists on the list.
 //Returns true when country exist, and false when it doesn't
 func AlreadyExistById(id int) bool {
 	_, err := GetCountryByID(id)
@@ -166,7 +195,7 @@ func AlreadyExistById(id int) bool {
 	return true
 }
 
-//Validate if the country already exists on the list
+//Validate if the country already exists on the list.
 //Returns true when the country exists and false when it doesn't
 func AlreadyExistByCode(code string) bool {
 	for _, c := range countries {
@@ -177,6 +206,44 @@ func AlreadyExistByCode(code string) bool {
 	return false
 }
 
+//Updates the hashmap containing all the countries to work with them in memory.
+func updateCountriesInMemory() int {
+	client, err := db.OpenConnectionToMongo()
+
+	if err != nil {
+		return -1
+	}
+
+	filter := bson.D{}
+	projection := bson.D{{"ID",1},{"Name", 1},{"Code", 1}}
+	opts := options.Find().SetProjection(projection)
+
+	coll := client.Database(db.GetDatabaseName()).Collection("Countries")
+	cursor, err := coll.Find(context.TODO(), filter, opts)
+
+	var results []bson.D
+
+	if err = cursor.All(context.TODO(), &results); err != nil {
+		panic(err)
+	}
+
+	defer db.CloseConnectionToMongo(client)
+
+	biggestId := 1
+	for _, v := range results {
+		c := bsonToCountry(v)
+		//add object into hashmap
+		countries[c.ID] = &c
+		if c.ID > biggestId{
+			biggestId = c.ID
+		}
+	}
+
+	return biggestId+1
+}
+
+//Receives a bson object to execute the conversion.
+//Returns a Country object.
 func bsonToCountry(v bson.D) Country {
 	bsonBytes, _ := bson.Marshal(v)
 
